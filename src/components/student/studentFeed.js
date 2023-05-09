732679a975a1a910606dc1fcb2816e6a72d70dd0
Siddhant <siddhantsonawane37@gmail.com>
2023-05-09 19:49:34 +0530

    comment

--- src/components/student/studentFeed.js
+++ src/components/student/studentFeed.js
@@ -2,9 +2,12 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "../post.css";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import Comment from "../comment";
 const BASE_URI = "http://localhost:3000";
 
-const StudentFeed = ({ userClass, userGroup }) => {
+const StudentFeed = ({ userId, userClass, userGroup }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -24,9 +27,9 @@ const StudentFeed = ({ userClass, userGroup }) => {
         {posts.map((post) => (
           <div className="post" key={post._id}>
             <div className="user">
-              {/* <div className="icon">
+              <div className="icon">
                 <img src={post.user.profilePic}></img>
-              </div> */}
+              </div>
               <div className="username">{post.user.name}</div>
             </div>
 
@@ -43,6 +46,30 @@ const StudentFeed = ({ userClass, userGroup }) => {
             </div>
 
             <div className="time">
+              <Popup
+                trigger={<p>Comments</p>}
+                modal
+                nested
+                contentStyle={{
+                  borderRadius: "10px",
+                  backgroundColor: "azure",
+                  fontFamily: "sans-serif",
+                  padding: "10px",
+                  width: "90%",
+                }}
+                className="popup-content"
+              >
+                {(close) => (
+                  <Comment
+                    close={close}
+                    postId={post._id}
+                    userId={userId}
+                    userClass={userClass}
+                    userGroup={userGroup}
+                  />
+                )}
+              </Popup>
+
               <p>
                 {new Date(post.createdAt).toLocaleString("default", {
                   day: "numeric",
---
+++ src/components/topbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="top-bar">
+      <div className="header">
+        <h1>TG System</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
